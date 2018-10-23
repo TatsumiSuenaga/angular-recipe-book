a074c2aadd1059015e5d5a68bf4d78aac28f9100
tsuenaga <tsuenaga@ibm.com>
2018-10-23 14:32:07 -0400

    Basics of the project is complete, section 3

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Test description', 'https://images.media-allrecipes.com/userphotos/600x600/1060867.jpg')
+  ];
 
   constructor() { }
 
